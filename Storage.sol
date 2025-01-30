7bbdfeb5b5949acf5b209c22f37fd29c2f8dc3cf
iliarusskikh <86166676+iliarusskikh@users.noreply.github.com>
2025-01-30 18:09:40 +0000

    Storage contract, retrieve the value

---
+++ Storage.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract Storage {
+
+    uint256 number;
+
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+}
+
+//public - all can access
+//external - Cannot be accessed internally, only externally
+//internal - only this contract and contracts deriving from it can access
+//private - can be accessed only from this contract
